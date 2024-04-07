3a00db92591ffc5226f37ccccae5e25b10d55e9c
AshanHerath <87167360+AshanHerath@users.noreply.github.com>
2024-04-07 10:22:50 +0530

    Create SpecificationController.java

---
+++ src/main/java/com/as/techheaven/specificationserviceapi/controller/SpecificationController.java
@@ -0,0 +1,47 @@
+package com.as.techheaven.specificationserviceapi.controller;
+
+import com.as.techheaven.specificationserviceapi.dto.request.RequestSpecificationDto;
+import com.as.techheaven.specificationserviceapi.dto.response.ResponseSpecificationDto;
+import com.as.techheaven.specificationserviceapi.service.SpecificationService;
+import com.as.techheaven.specificationserviceapi.util.StandardResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/specifications")
+@RequiredArgsConstructor
+public class SpecificationController {
+
+    private final SpecificationService specificationService;
+
+    @PostMapping
+    private ResponseEntity<StandardResponse> createSpecification(@RequestBody RequestSpecificationDto dto){
+
+        specificationService.createSpecification(dto);
+        return new ResponseEntity<>(new StandardResponse(201, "Specification was created successfully", dto.getSpec()), HttpStatus.CREATED);
+
+    }
+
+    @GetMapping("/byCategory/{categoryId}")
+    @ResponseStatus(HttpStatus.OK)
+    private List<ResponseSpecificationDto> getSpecificationsByCategoryId(@PathVariable long categoryId){
+        return specificationService.findSpecificationByCategoryId(categoryId);
+    }
+
+    @GetMapping("/{ids}")
+    @ResponseStatus(HttpStatus.OK)
+    private boolean isListAvailable(@PathVariable List<String> ids){
+        return specificationService.isListAvailable(ids);
+    }
+
+    @PostMapping("/saveList")
+    @ResponseStatus(HttpStatus.CREATED)
+    private void saveSpecifications(@RequestBody List<RequestSpecificationDto> dtos){
+        specificationService.saveSpecifications(dtos);
+    }
+
+}
