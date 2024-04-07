69990468e682db7c37e1176e4b83a4348edd6ce9
AshanHerath <87167360+AshanHerath@users.noreply.github.com>
2024-04-07 09:52:22 +0530

    Create specification-service-api

---
+++ src/main/java/com/as/techheaven/specificationserviceapi/SpecificationServiceApiApplication.java
@@ -0,0 +1,13 @@
+package com.as.techheaven.specificationserviceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpecificationServiceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpecificationServiceApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/as/techheaven/specificationserviceapi/SpecificationServiceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.as.techheaven.specificationserviceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpecificationServiceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
