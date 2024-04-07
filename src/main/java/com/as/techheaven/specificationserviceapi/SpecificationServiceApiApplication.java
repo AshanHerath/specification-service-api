d59898248f4066406072db073cf23dce31958a96
AshanHerath <87167360+AshanHerath@users.noreply.github.com>
2024-04-07 11:28:11 +0530

    configuration with eureka client server

--- src/main/java/com/as/techheaven/specificationserviceapi/SpecificationServiceApiApplication.java
+++ src/main/java/com/as/techheaven/specificationserviceapi/SpecificationServiceApiApplication.java
@@ -2,8 +2,10 @@ package com.as.techheaven.specificationserviceapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class SpecificationServiceApiApplication {
 
 	public static void main(String[] args) {
